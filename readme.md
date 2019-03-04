f47f7799838268b741c7af802878d05ab67edc9d
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-04 17:44:44 +0100

    wip

--- readme.md
+++ readme.md
@@ -12,3 +12,9 @@
 We need `node` & `npm`, as well as packages `live-server` and `eslint` installed _globally_. VSCode's `eslint` extension is recommended. Command line commands are run inside the project folder. This guide uses npm but should work the same using yarn.
 
 # Steps
+
+## App.jsx
+  * Explain the different moving parts.
+  * Flesh out one reducer, let students do the other.
+  * Ask the students to use `combineReducers`.
+  * Flesh out two action creators, let students do the other.
--- src/App.jsx
+++ src/App.jsx
@@ -6,73 +6,60 @@ import uuid from 'uuid';
 import Container from './components/Container';
 import dummyQuotes from './constants/dummyQuotes';
 
-// ACTION TYPES
+// =================================================================== ACTION TYPES (pull out)
 export const DELETE_QUOTE = 'DELETE_QUOTE';
 export const MAKE_QUOTE_OF_THE_DAY = 'MAKE_QUOTE_OF_THE_DAY';
 export const ADD_QUOTE = 'ADD_QUOTE';
 
-// ACTION CREATOR deleteQuote
-export function deleteQuote(id) {
-  return {
-    type: DELETE_QUOTE,
-    payload: id,
-  };
-}
-
-// ACTION CREATOR makeQuoteOfTheDay
-export function makeQuoteOfTheDay(id) {
-  return {
-    type: MAKE_QUOTE_OF_THE_DAY,
-    payload: id,
-  };
-}
-
-// ACTION CREATOR addQuote
-export function addQuote(author, text) {
-  return {
-    type: ADD_QUOTE,
-    payload: {
-      id: uuid(),
-      author,
-      text,
-    },
-  };
-}
-
+// =================================================================== REDUCERS  (pull out)
 // REDUCER quotes
 function quotes(state = dummyQuotes, action) {
-  switch (action.type) {
-    case ADD_QUOTE:
-      return state.concat(action.payload);
-    case DELETE_QUOTE:
-      return state.filter(quote => quote.id !== action.payload);
-    default:
-      return state;
-  }
+  // 1- flesh out using action types as your guide
+  return state;
 }
 
 // REDUCER quoteOfTheDay
 function quoteOfTheDay(state = null, action) {
-  switch (action.type) {
-    case MAKE_QUOTE_OF_THE_DAY:
-      return action.payload;
-    case DELETE_QUOTE:
-      return state === action.payload ? null : state;
-    default:
-      return state;
-  }
+  // 2- flesh out using action types as your guide
+  return state;
 }
 
-const rootReducer = combineReducers({
-  quotes,
-  quoteOfTheDay,
-});
+// =================================================================== COMBINE REDUCERS
+// 3- use combineReducers to create a root reducer off of our reducers
+const rootReducer = () => ({ quotes: dummyQuotes, quoteOfTheDay: null });
 
+// =================================================================== ACTION CREATORS  (pull out)
+// 4- ACTION CREATOR deleteQuote
+export function deleteQuote(id) {
+  return {
+    // what type?
+    // what payload?
+  };
+}
+
+// 5- ACTION CREATOR makeQuoteOfTheDay
+export function makeQuoteOfTheDay(id) {
+  return {
+    // what type?
+    // what payload?
+  };
+}
+
+// 6- ACTION CREATOR addQuote
+export function addQuote(author, text) {
+  return {
+    // what type?
+    // what payload?
+  };
+}
+
+// =================================================================== CREATE THE STORE
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
+// =================================================================== INJECT THE STORE
 ReactDOM.render(
   <Provider store={store}>
     <Container />
